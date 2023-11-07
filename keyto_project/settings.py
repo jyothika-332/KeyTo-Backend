ec533afbaba6230cb36930492023face591ec361
jyothika-332 <jyothikact10@gmail.com>
2023-11-07 11:38:12 +0530

    redis added to .env

--- keyto_project/settings.py
+++ keyto_project/settings.py
@@ -161,7 +161,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('localhost', 6379)],
+            "hosts": [(config('redis_host'))],
         },
     },
 }
