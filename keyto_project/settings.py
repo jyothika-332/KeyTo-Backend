36e598b016f11f19a2d8162f9d447db97c7802af
jyothika-332 <jyothikact10@gmail.com>
2023-11-16 11:59:16 +0530

    cors updated1

--- keyto_project/settings.py
+++ keyto_project/settings.py
@@ -220,6 +220,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'userapp.User'
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'https://key-to-frontend.vercel.app',
